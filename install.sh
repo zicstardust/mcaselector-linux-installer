aba9f4f43a3cf290276b55659f10c32eeaaca2d9
Zic <git@zicstardust.com>
2024-09-28 10:34:23 -0400

    update jre 17 to 21

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ else
     install_dir=${HOME}/.local/share/mcaselector
 fi
 
-jre_version=zulu17.44.53-ca-fx-jre17.0.8.1
+jre_version=zulu21.36.17-ca-fx-jre21.0.4
 mcaselector_latest_version=$(wget github.com/Querz/mcaselector/releases/latest -q -O - | grep "<title>" | grep -o '[0-9]*[.][0-9]*[.]*[0-9]*\+')
 
 mkdir -p ${install_dir}
